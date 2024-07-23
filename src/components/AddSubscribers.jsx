84cba3c9eab81106eedfe3a8641b84302892b519
joel-tjohn <joeltjohn10@gmail.com>
2024-07-23 20:18:32 +0530

    Add Subscriber Created

---
+++ src/components/AddSubscribers.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Title from './Title'
+
+const AddSubscribers = () => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+<Title/>
+
+                     
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Subscriber Name</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Phone Number</label>
+                                <input type="text" className="form-control" />
+                            </div>
+
+
+
+                        </div>
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">Address</label>
+                                <textarea name="" id="" className="form-control"></textarea>
+                            </div>
+                        </div>
+
+                        <div className="row g-3">
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Pincode</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">District</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Village Office / Muncipality</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Email</label>
+                                <input type="email" name="" id="" className="form-control" />
+                            </div>
+
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+
+                                <button className="btn btn-dark">Submit</button>
+
+                            </div>
+                        </div>
+
+
+
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+
+
+
+    )
+}
+
+export default AddSubscribers
---
+++ src/components/Title.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div>
+
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+
+                                <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/reading-newspaper-1502520-1272730.png" width="100" height="100" alt="..." class="img-thumbnail" />
+
+                                
+                                   
+                                <h1 class=" text-end display-1" ><strong><mark><u>THE READER</u></mark></strong></h1>
+                                  
+                                
+                            </div>
+                        </div>
+
+
+
+
+                        <br /><br /><br /><br /><br />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Title
